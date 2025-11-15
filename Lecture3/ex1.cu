b23c313e386a43541c1a05f430ff8fc763d0299f
LivanKov <ilomakov4@gmail.com>
2025-11-15 15:58:48 +0100

    first example, lecture 3

---
+++ Lecture3/ex1.cu
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__void hello_from_gpu() {
+    int idx = blockIdx.x * 4 + threadIdx.x;
+    printf("GPU block %d, thread %d, idx %d\n",
+        blockIdx.x, threadIdx.x, idx);
+}
+
+int main() {
+    printf("CPU says hello before kernel!\n");
+
+    // Launch kernel
+    hello_from_gpu<<<3, 4>>>();
+
+    // Wait for GPU to finish
+    cudaDeviceSynchronize();
+
+    printf("CPU says hello after kernel!\n");
+
+    return 0;
+}
